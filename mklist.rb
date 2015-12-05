22c66c9b03e4b8835aab7f6678c6f0cc7736eaa8
Baptiste Fontaine <batifon@yahoo.fr>
2015-12-05 18:02:53 +0100

    minor refactoring

--- mklist.rb
+++ mklist.rb
@@ -5,7 +5,32 @@ require "erb"
 require "ostruct"
 require "yaml"
 
+def day_title(day)
+  "#{day == 1 ? "1er" : day} Décembre"
+end
+
 class Book
+  TEMPLATE = <<-EOS
+<article class="book">
+  <h1><span class="title"><%= title.strip %></span>, par <span class="author"><%= author.strip %></span><% if link %>&nbsp;(<a href="<%= link %>">lien</a>)<% end %></h1>
+  <div class="sources">
+  <% if source %>
+    <blockquote class="comment">
+      <%= comment.strip.sub('\n', "<br/>") %><br/>
+      <span class="comment-author">—&nbsp;&nbsp;<a class="handle" href="<%= source %>">@<%= source.split("/")[3] %></a></span>
+    </blockquote>
+  <% elsif sources %>
+    <% for s in sources %>
+      <blockquote class="comment">
+        <%= s["comment"].strip.sub('\n', "<br/>") %><br/>
+        <span class="comment-author">—&nbsp;&nbsp;<a class="handle" href="<%= s["link"] %>">@<%= s["link"].split("/")[3] %></a></span>
+      </blockquote>
+    <% end %>
+  <% end %>
+  </div>
+</article>
+  EOS
+
   def initialize(hash, day, idx)
     @attrs = hash
     @day = day
@@ -13,7 +38,7 @@ class Book
   end
 
   def to_binding
-    # https://bugs.ruby-lang.org/issues/8643
+    # ERB wants bindings; not hashs. See https://bugs.ruby-lang.org/issues/8643
     OpenStruct.new(@attrs).instance_eval { binding }
   end
 
@@ -56,34 +81,9 @@ class Book
   end
 end
 
-def day_title(day)
-  "#{day == 1 ? "1er" : day} Décembre"
-end
-
 days = YAML.load_file("books.yml")
 
-book_template = <<-EOS
-<article class="book">
-  <h1><span class="title"><%= title.strip %></span>, par <span class="author"><%= author.strip %></span><% if link %>&nbsp;(<a href="<%= link %>">lien</a>)<% end %></h1>
-  <div class="sources">
-  <% if source %>
-    <blockquote class="comment">
-      <%= comment.strip.sub('\n', "<br/>") %><br/>
-      <span class="comment-author">—&nbsp;&nbsp;<a class="handle" href="<%= source %>">@<%= source.split("/")[3] %></a></span>
-    </blockquote>
-  <% elsif sources %>
-    <% for s in sources %>
-      <blockquote class="comment">
-        <%= s["comment"].strip.sub('\n', "<br/>") %><br/>
-        <span class="comment-author">—&nbsp;&nbsp;<a class="handle" href="<%= s["link"] %>">@<%= s["link"].split("/")[3] %></a></span>
-      </blockquote>
-    <% end %>
-  <% end %>
-  </div>
-</article>
-EOS
-
-renderer = ERB.new(book_template, 3)
+renderer = ERB.new(Book::TEMPLATE, 3)
 
 File.open("index.md", "w") do |f|
   f.write <<-EOS
